c51ed3f1b7c5c292895cd5e48e833b7ddb157c08
Hades <marcelli335gamer@gmail.com>
2025-09-13 14:04:57 -0300

    (forgot to remove the chatgpt comments, k)

--- carol-discord-bot/src/main/java/com/marc/discordbot/carol/commands/CarolCommandInitializer.java
+++ carol-discord-bot/src/main/java/com/marc/discordbot/carol/commands/CarolCommandInitializer.java
@@ -7,12 +7,11 @@ public class CarolCommandInitializer {
     public static void initializeCommands() { // i admit, this was made by ChatGPT
         Reflections reflections = new Reflections("com.marc.discordbot.carol.commands");
 
-        // Pega todas as classes que estendem CarolCommand
         Set<Class<? extends CarolCommand>> classes = reflections.getSubTypesOf(CarolCommand.class);
 
         for (Class<? extends CarolCommand> clazz : classes) {
             try {
-                clazz.getDeclaredConstructor().newInstance(); // instancia a classe
+                clazz.getDeclaredConstructor().newInstance();
             } catch (Exception e) {
                 e.printStackTrace();
             }
