b8298d4563206cfa1225f962bfe927d9181e7223
Hades <marcelli335gamer@gmail.com>
2025-09-23 20:37:20 -0300

    rickroll added to voice player 3000 command

--- carol-discord-bot/src/main/java/com/marc/discordbot/carol/voice/CarolVoiceSoundList.java
+++ carol-discord-bot/src/main/java/com/marc/discordbot/carol/voice/CarolVoiceSoundList.java
@@ -2,6 +2,7 @@ package com.marc.discordbot.carol.voice;
 
 public class CarolVoiceSoundList {
     public static CarolVoiceMemeSound[] sounds = new CarolVoiceMemeSound[]{
-            new CarolVoiceMemeSound("wait","Wait! They don't love you like i love you!")
+            new CarolVoiceMemeSound("wait","Wait! They don't love you like i love you!"),
+            new CarolVoiceMemeSound("rickroll", "Never gonna give you up!")
     };
 }
