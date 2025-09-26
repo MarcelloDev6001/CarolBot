f1ec05e1107e6a302bc4e11d5715d9b9d6a467cb
Hades <marcelli335gamer@gmail.com>
2025-09-26 19:17:48 -0300

    changed commentary file, kk

--- carol-discord-bot/src/main/java/com/marc/discordbot/carol/cache/CarolCacheManager.java
+++ carol-discord-bot/src/main/java/com/marc/discordbot/carol/cache/CarolCacheManager.java
@@ -9,6 +9,12 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+// soo, I'll try to explain how this works:
+// when I try to pull a guild from the database, everytime is a new read, and this will consume alot of read
+// so to reduce read, we cache the guild and user, and then we update it every X minutes
+// when you try to pull using any get method from CarolDatabaseManager, it firstly tries to get from the cache
+// if not found, then it gets from the database
+// IDK if this is a good system, but I hope it is
 public class CarolCacheManager {
     public static void initialize() {
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
--- carol-discord-bot/src/main/java/com/marc/discordbot/carol/cache/CarolDatabaseCache.java
+++ carol-discord-bot/src/main/java/com/marc/discordbot/carol/cache/CarolDatabaseCache.java
@@ -11,12 +11,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-// soo, I'll try to explain how this works:
-// when I try to pull a guild from the database, everytime is a new read, and this will consume alot of read
-// so to reduce read, we cache the guild and user, and then we update it every X minutes
-// when you try to pull using any get method from CarolDatabaseManager, it firstly tries to get from the cache
-// if not found, then it gets from the database
-// IDK if this is a good system, but i hope it is
 public class CarolDatabaseCache {
     public static List<CarolDatabaseUser> dbUsersCached = new ArrayList<>();
     public static List<CarolDatabaseGuild> dbGuildsCached = new ArrayList<>();
