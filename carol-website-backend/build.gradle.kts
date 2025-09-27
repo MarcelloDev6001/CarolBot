85ebb3a21159cfaf9fbbef32590d2a7bd925761e
Hades <marcelli335gamer@gmail.com>
2025-09-27 15:26:36 -0300

    backend test

---
+++ carol-website-backend/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.3.2"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+java {
+    toolchain {
+        // why can't i use java 24 here? ;-;
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+
+group = "com.marc.discordbot.carol.website.backend"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ carol-website-backend/src/main/java/com/marc/discordbot/carol/website/backend/CarolBackendApplication.java
@@ -0,0 +1,11 @@
+package com.marc.discordbot.carol.website.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarolBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CarolBackendApplication.class, args);
+    }
+}
